9797048dfb049705b09c061394d64154133acb9c
Your N <salhiali197@gmail.com>
2020-12-13 16:19:24 +0100

    add changes 4

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-# -*- coding: utf8 -*
+# # -*- coding: utf8 -*
 import pyttsx3
+# engine = pyttsx3.init()
+# voice = engine.getProperty('voices')[0] # the french voice
+# engine.setProperty('voice', voice.id)
+
+# engine.say('Je lui ai dit, fait gaffe a ton nez') # perfect
+
+# #engine.say('Tu as bien mangé?') # it works!!
+
+# engine.runAndWait()    
+
 engine = pyttsx3.init()
-voice = engine.getProperty('voices')[0] # the french voice
-engine.setProperty('voice', voice.id)
-
-engine.say('Je lui ai dit, fait gaffe a ton nez') # perfect
-
-#engine.say('Tu as bien mangé?') # it works!!
-
-engine.runAndWait()    
+voices = engine.getProperty('voices')
+for voice in voices:
+   engine.setProperty('voice', voice.id)
+   engine.say('The quick brown fox jumped over the lazy dog.')
+engine.runAndWait()
