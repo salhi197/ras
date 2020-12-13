f4b0afaaa1cd426d24184eb915c11c6c248d1935
Your N <salhiali197@gmail.com>
2020-12-13 14:34:25 +0100

    add changes

--- tts.py
+++ tts.py
@@ -1,7 +1,7 @@
 import os
 cmd_beg = 'espeak '
 ticket  = input('Entrer le numero de ticket : ') 
-text    = "ticket numero"+ticket+" va au gichet "+gichet
+# text    = "ticket numero"+ticket+" va au gichet "+gichet
 os.system(cmd_beg+str(ticket)) 
 # from num2word import num2words
 # from subspaces import call
