c792dc280c236fa62ade94ad5968ffd71ea0b342
Your N <salhiali197@gmail.com>
2020-12-13 16:33:42 +0100

    add changes 4

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*
 import pyttsx3
 engine = pyttsx3.init()
 engine.say("Sac à merde. Je sais que tu te tapes ma femme. Je ne vais donc pas prévoir de catégorie Mathématiques à mes récompenses. Signé : Monsieur Nobel.")
