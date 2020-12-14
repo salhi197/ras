4e765d82625b2a58b67ef0a7628a85ec2e37eaf9
Your N <salhiali197@gmail.com>
2020-12-14 09:45:09 +0100

    add changes second day

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 # -*- coding: utf8 -*
-import pyttsx3
-engine = pyttsx3.init()
-voice = engine.getProperty('voices')[0] # the french voice
-engine.setProperty('voice', voice.id)
-
-engine.say('Siiifou ') # it works!!
-
-engine.runAndWait()    
+import pyttsx3;
+engine = pyttsx3.init();
+engine.say("I will speak this text");
+engine.runAndWait() ;
