7e5dd11eef4f4c18cd4229e2f2f574ca19761e44
Your N <salhiali197@gmail.com>
2020-12-14 15:39:45 +0100

    add slash

--- tts.py
+++ tts.py
@@ -6,7 +6,7 @@ ticket  = input('Entrer le numero de ticket : ')
 # text    = "ticket numero"+ticket+" va au gichet "+gichet
 os.system("aplay _phrase.wav") 
 #command = '-vfr+f'+str(f)+' -z -a50 -s'+str(speed)+' "ff on apelle le ticket numero  '+str(ticket)+'"'
-audio_number = "aplay "+ticket+".wav"
+audio_number = "aplay ./audios/"+ticket+".wav"
 #command = '-vfr+f3 -z -a50 -s120 "'+str(ticket)+'"'
 #os.system(cmd_beg+command)
 os.system(audio_number)
