7bab340c40d4a36b3ab17e7f9faa47e89f43895d
Your N <salhiali197@gmail.com>
2020-12-14 15:20:20 +0100

    add wav

--- tts.py
+++ tts.py
@@ -1,12 +1,10 @@
 import os
 cmd_beg = 'sudo espeak '
 speed = 120
-
 ticket  = input('Entrer le numero de ticket : ') 
-f  = input('frequence : ') 
+#f  = input('frequence : ') 
 # text    = "ticket numero"+ticket+" va au gichet "+gichet
 command = '-vfr+f'+str(f)+' -z -a50 -s'+str(speed)+' "ff on apelle le ticket numero  '+str(ticket)+'"'
-
 os.system(cmd_beg+command) 
 # from num2word import num2words
 # from subspaces import call
