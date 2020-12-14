07881772c9c17724c2aebd37d49cef54ab9342ac
Your N <salhiali197@gmail.com>
2020-12-14 11:09:29 +0100

    before final push

--- tts.py
+++ tts.py
@@ -1,8 +1,9 @@
 import os
 cmd_beg = 'espeak '
+
 ticket  = input('Entrer le numero de ticket : ') 
 # text    = "ticket numero"+ticket+" va au gichet "+gichet
-os.system(cmd_beg+ticket+" --voices") 
+os.system(cmd_beg+'-vfr+f4 -s120 "ff le ticket numero    '+str(ticket)) 
 # from num2word import num2words
 # from subspaces import call
 # cmd_beg = 'espeak '
