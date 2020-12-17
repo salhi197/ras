c6b4d33d4b13cfcc95efb840ff93e4efe9e53a36
Your N <salhiali197@gmail.com>
2020-12-17 10:58:52 +0100

    remove files

--- num2words.py
+++
@@ -1,4 +0,0 @@
-from num2words import num2words
-num2words(42)
-num2words(42, to='ordinal')
-num2words(42, lang='fr')
--- test.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf8 -*
-import pyttsx3;
-engine = pyttsx3.init();
-engine.say("  45 ");
-engine.runAndWait() ;
